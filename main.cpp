b7066aee7f81f48973de1eb9d7366a7b9e479ed3
onukiyu <k023g0017@g.neec.ac.jp>
2025-02-07 14:34:29 +0900

    PG3_01_03 確認課題 完了

--- main.cpp
+++ main.cpp
@@ -1,10 +1,48 @@
 #include <stdio.h>
-#include<Windows.h>
+#include <stdlib.h>
+#include <time.h>
+#include <windows.h>
 
-int main() {
-	SetConsoleOutputCP(65001);
+    // コールバック関数のプロトタイプ宣言
+    typedef void (*Callback)(int result);
 
-	printf("��������");
+    // サイコロの出目を決定する関数
+    int roll_dice() {
+        return rand() % 6 + 1;
+    }
 
-	return 0;
-}
+    // 判定を行うコールバック関数
+    void judge_result(int result) {
+
+        int input;
+
+        printf("サイコロの出目が奇数(1)か偶数(2)か入力:");
+        scanf_s("%d", &input);
+
+        printf("判定中...\n");
+
+        Sleep(3000);
+
+        if ((result % 2 == 1 && input == 1) || (result % 2 == 0 && input == 2)) {
+            printf("正解\n");
+        }
+        else {
+            printf("不正解\n");
+        }
+
+        printf("サイコロの目は%d\n", result);
+    }
+
+    // メイン関数
+    int main() {
+
+        srand((int)time(NULL));
+
+        int dice_result = roll_dice();
+
+        Callback callback = judge_result;
+
+        callback(dice_result);
+
+        return 0;
+    }
